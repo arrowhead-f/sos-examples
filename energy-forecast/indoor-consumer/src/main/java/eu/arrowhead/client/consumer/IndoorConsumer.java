a6ef1da60619259180652070e7de867c77d1f75b
Thomas Pedersen <tp@cs.aau.dk>
2018-11-15 10:53:31 +0100

    Better strategy naming

--- energy-forecast/indoor-consumer/src/main/java/eu/arrowhead/client/consumer/IndoorConsumer.java
+++ energy-forecast/indoor-consumer/src/main/java/eu/arrowhead/client/consumer/IndoorConsumer.java
@@ -39,7 +39,7 @@ public class IndoorConsumer extends ArrowheadApplication {
                 .flag(OrchestrationFlags.Flags.METADATA_SEARCH, true)
                 .flag(OrchestrationFlags.Flags.ENABLE_INTER_CLOUD, false)
                 .build();
-        final OrchestrationStrategy strategy = new OrchestrationStrategy.StaticOrch(orchestrationClient, serviceRequestForm);
+        final OrchestrationStrategy strategy = new OrchestrationStrategy.Once(orchestrationClient, serviceRequestForm);
         final HttpClient indoorClient = new HttpClient(strategy, getProps().isSecure(), securityContext);
         final Message message = indoorClient.request(HttpClient.Method.GET).readEntity(Message.class);
         System.out.println("Provider Response payload: " + Utility.toPrettyJson(null, message));
--- energy-forecast/outdoor-consumer/src/main/java/eu/arrowhead/client/consumer/OutdoorConsumer.java
+++ energy-forecast/outdoor-consumer/src/main/java/eu/arrowhead/client/consumer/OutdoorConsumer.java
@@ -40,7 +40,7 @@ public class OutdoorConsumer extends ArrowheadApplication {
                 .flag(OrchestrationFlags.Flags.METADATA_SEARCH, true)
                 .flag(OrchestrationFlags.Flags.ENABLE_INTER_CLOUD, false)
                 .build();
-        final OrchestrationStrategy strategy = new OrchestrationStrategy.StaticOrch(orchestrationClient, serviceRequestForm);
+        final OrchestrationStrategy strategy = new OrchestrationStrategy.Once(orchestrationClient, serviceRequestForm);
         final HttpClient outdoorClient = new HttpClient(strategy, getProps().isSecure(), securityContext);
         final Message message = outdoorClient.request(HttpClient.Method.GET).readEntity(Message.class);
         log.info("Got " + message.getEntry().size() + " entries.");
